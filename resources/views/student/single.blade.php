660a7f498fd05d665c2b44a16ac427c66fdbb2bb
Antoine07 <antoine.lucsko@wanadoo.fr>
2015-09-01 15:41:18 +0200

    balde introduction

--- resources/views/student/single.blade.php
+++ resources/views/student/single.blade.php
@@ -1,6 +1,16 @@
+@extends('layouts.master')
+
+@section('title')
+    @if(!empty($student))
+        {{$student->username}}
+    @endif
+@stop
+
+@section('content')
 @if(!empty($student))
     <h2>{{$student->username}}</h2>
     <p>{{$student->email}}</p>
 @else
     <p>Désolé pas de student</p>
 @endif
+@endsection
