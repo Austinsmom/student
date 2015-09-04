cd44929d3694faedc7a1eb486b3857552630723e
Antoine07 <antoine.lucsko@wanadoo.fr>
2015-09-04 16:09:38 +0200

    helper form

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -2,12 +2,12 @@
 <p>
 {!! Form::label('name', 'Name:', ['for'=>'name']) !!}
 {!! Form::text('name', old('name'), ['id'=>'name'] ) !!}
-{!! $errors->firts('name', '<span class="error">:message</span>') !!}
+{!! $errors->first('name', '<span class="error">:message</span>') !!}
 </p>
 <p>
 {!! Form::label('email', 'Email:', ['for'=>'email']) !!}
 {!! Form::text('email') !!}
-{!! $errors->firts('name', '<span class="error">:message</span>') !!}
+{!! $errors->first('email', '<span class="error">:message</span>') !!}
 </p>
 {!! Form::submit('create') !!}
 {!! Form::close() !!}
