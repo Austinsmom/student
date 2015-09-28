efee6cd4e62dc0f0526a1c3fe4b62c09b4f34c40
Antoine07 <antoine.lucsko@wanadoo.fr>
2015-09-28 14:23:19 +0200

    posts

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master_admin')
+
+@section('content')
+    <div class="pagination">
+        {!!$posts->render()!!}
+    </div>
+    <table class="table table-hover table-bordered">
+        <thead>
+        <tr>
+            <th>Status</th>
+            <th>Voir</th>
+            <th>titre</th>
+            <th>date publication</th>
+            <th>delete</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($posts as $post)
+            <tr class="{{($post->status=='published')? 'success' : 'info'}}">
+                <td>{{$post->status}}</td>
+                <td><a href="{{url('post/'.$post->id)}}"><span
+                                class="glyphicon glyphicon-eye-open"></span></a></td>
+                <td><span class="glyphicon glyphicon-edit"></span> <a
+                            href="{{url('post/'.$post->id.'/edit')}}">{{$post->title}}</a></td>
+                <td>{{$post->published_at}} </td>
+                <td>{!! Form::open(['url'=>'post/'.$post->id, 'method'=>'DELETE', 'class'=>'form-delete']) !!}
+                    <div class="form-group">
+                        {!! Form::submit('delete', ['class'=>'btn']) !!}
+                    </div>
+                    {!! Form::close() !!}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    <div class="pagination">
+        {!!$posts->render()!!}
+    </div>
+@stop
