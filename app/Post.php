c57697d497dfb153d63dc0715e26e7c6677abfc9
Antoine07 <antoine.lucsko@wanadoo.fr>
2015-09-29 12:36:54 +0200

    model post with accessor for datetime

--- app/Post.php
+++ app/Post.php
@@ -2,23 +2,32 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-   protected $fillable=[
-       'title',
-       'content',
-       'category_id'
-   ];
+    protected $fillable = [
+        'title',
+        'content',
+        'category_id',
+        'published_at',
+        'status'
+    ];
 
-   public function category()
-   {
-       return $this->belongsTo('App\Category');
-   }
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 
     public function comments()
     {
         return $this->hasMany('App\Comment');
+    }
+
+    public function setPublishedAtAttribute($value)
+    {
+        $now = Carbon::now();
+        $this->attributes['published_at'] = "$value {$now->hour}:{$now->minute}:{$now->second}";
     }
 }
