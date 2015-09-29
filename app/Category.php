2c1bc1691bfaee39416d3392d5fc11a8112490d1
Antoine07 <antoine.lucsko@wanadoo.fr>
2015-09-29 11:07:31 +0200

    Model Category

--- app/Category.php
+++ app/Category.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable=[
+    protected $fillable = [
         'title'
     ];
 
     public function posts()
     {
         return $this->hasMany('App\Post');
+    }
+
+    public function scopeForCreate($query)
+    {
+        return $query->select('title', 'id');
     }
 }
